4e925e285ee3198879f7f088803517ed84eebe28
Plasmatic <moses@mosesxu.net>
2020-12-05 14:39:48 -0500

    comment with better day5 solution

--- day5p1.rb
+++ day5p1.rb
@@ -1,6 +1,9 @@
 require_relative 'funs'
 require 'scanf'
 
+# An approach like
+# ans = data.map { |s| s.chars.reduce(0) { |pre, c| pre * 2 + (c == "R" || c == "B" ? 1 : 0) }}.max
+# would probably work much better but I didn't think of it in time
 data = load_data(5).split("\n")
 ans = data.map { |s|
     8 * s[0..6].chars.each_with_index.map { |x, i| x == "B" ? 1 << (6 - i) : 0 }.sum +
--- day5p2.rb
+++ day5p2.rb
@@ -2,6 +2,9 @@ require_relative 'funs'
 require 'set'
 require 'scanf'
 
+# An approach like
+# ids = data.map { |s| s.chars.reduce(0) { |pre, c| pre * 2 + (c == "R" || c == "B" ? 1 : 0) }}
+# would probably work much better but I didn't think of it in time
 data = load_data(5).split("\n")
 ids = data.map { |s|
     8 * (s[0..6].chars.each_with_index.map { |x, i| x == "B" ? 1 << (6 - i) : 0 }.sum) +
